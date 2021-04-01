77da730619fdbc19f70b4bfb1479f17d6742807b
한상엽 <gkstkdduq1@gmail.com>
2021-04-01 14:11:41 +0900

    multi tickers

--- main.py
+++ main.py
@@ -9,51 +9,80 @@ secret_key = "BuXKRKaxcPdhL8Htpl1cGsVbqGh0zd10DHVLLxWB"
 
 upbit = pyupbit.Upbit(access_key, secret_key)
 
-tickers = pyupbit.get_tickers(fiat="KRW")
-
-ticker = 'KRW-MARO'
 balance = 0
 fee = 0.001
 ror = 1
 
-while True:
-    url = "https://api.upbit.com/v1/candles/minutes/1"
+url = "https://api.upbit.com/v1/ticker"
 
-    querystring = {"market": ticker, "count": "500"}
+tickers = pyupbit.get_tickers(fiat="KRW")
+trade_price_list = []
+change_list = []
 
+for ticker in tqdm(tickers):
+    querystring = {"markets": ticker}
     response = requests.request("GET", url, params=querystring)
-    data = response.json()
-    df = pd.DataFrame(data)
-    df = df.reindex(index=df.index[::-1]).reset_index()
-    df = df[-20:]
-    ma20 = df['trade_price'].mean()
-    df = df[-5:]
-    ma5 = df['trade_price'].mean()
-    df['range'] = (df['high_price'] - df['low_price']) * 0.5
-    df['target'] = df['opening_price'] + df['range'].shift(1)
+    # print(ticker)
+    # print(response.json()[0]['acc_trade_price_24h'])
+    time.sleep(0.1)
+    trade_price_list.append(response.json()[0]['acc_trade_price_24h'])
+    change_list.append(response.json()[0]['change'])
 
-    df = df[-1:]
+trade_price_df = pd.DataFrame(list(zip(trade_price_list, change_list)), index=tickers,
+                              columns=['trade_price', 'change'])
+trade_price_df.sort_values(by=['trade_price'], ascending=False, inplace=True)
+ticker_list = trade_price_df.iloc[:10].index.tolist()
+print(ticker_list)
 
-    cur_price = pyupbit.get_current_price(ticker)
-    bull_flag = cur_price > ma5 and cur_price > ma20
-    target_price = df['target'].values[0]
-    signal = cur_price > target_price and bull_flag
+while True:
+    for ticker in ticker_list:
+        signal = 1
+        while signal:
+            url = "https://api.upbit.com/v1/candles/minutes/1"
 
-    if cur_price > target_price and bull_flag and balance == 0:
-        buyprice = pyupbit.get_current_price(ticker)
-        balance = 1
+            querystring = {"market": ticker, "count": "500"}
 
-    if not bull_flag and balance == 1:
-        sellprice = pyupbit.get_current_price(ticker)
-        ror = ror * ((sellprice / buyprice) - fee)
-        balance = 0
-    if balance == 1:
-        ror_now = (cur_price/buyprice)-fee
-        print('signal', signal, ', Upbit 1 minute ', ticker, ', cur_price : ', cur_price, ', target: ', target_price,
-              ', bull: ', bull_flag, ', balance:', balance, ', ma:', ma5, ', ror:', round(ror_now, 4), end='\r')
-    if balance == 0:
-        print('signal', signal, ', Upbit 1 minute ', ticker, ', cur_price : ', cur_price, ', target: ', target_price,
-            ', bull: ', bull_flag, ', balance:', balance, ', ma:', ma5, ', ror:', round(ror, 4), end='\r')
+            response = requests.request("GET", url, params=querystring)
+            data = response.json()
+            df = pd.DataFrame(data)
+            df = df.reindex(index=df.index[::-1]).reset_index()
+            df = df[-20:]
+            ma20 = df['trade_price'].mean()
+            df = df[-5:]
+            ma5 = df['trade_price'].mean()
+            df['range'] = (df['high_price'] - df['low_price']) * 0.5
+            df['target'] = df['opening_price'] + df['range'].shift(1)
 
-    time.sleep(1)
+            df = df[-1:]
+
+            cur_price = pyupbit.get_current_price(ticker)
+            bull_flag = cur_price > ma5 and cur_price > ma20
+            target_price = df['target'].values[0]
+            if bull_flag:
+                signal = cur_price - target_price
+            else:
+                signal = 0
+
+            # buy here
+            if signal > 0 and bull_flag and balance == 0:
+                buyprice = pyupbit.get_current_price(ticker)
+                balance = 1
+
+            # sell here
+            if not bull_flag and balance == 1:
+                sellprice = pyupbit.get_current_price(ticker)
+                ror = ror * ((sellprice / buyprice) - fee)
+                balance = 0
+
+            if balance == 1:
+                ror_now = (cur_price / buyprice) - fee
+                print(', Upbit 1 minute ', ticker, ', ror:', round(ror_now, 4), 'signal', signal, ', cur_price : ',
+                      cur_price,
+                      ', target: ', target_price, ', bull: ', bull_flag, ', balance:', balance, end='\r')
+            if balance == 0:
+                print(', Upbit 1 minute ', ticker, ', ror:', round(ror, 4), 'signal', signal, ', cur_price : ',
+                      cur_price,
+                      ', target: ', target_price, ', bull: ', bull_flag, ', balance:', balance, end='\r')
+
+            time.sleep(0.5)
 
